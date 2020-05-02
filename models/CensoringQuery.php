e78832d2fd14fd2a05b22fda2cfbb2a657048518
denis909 <denis909@mail.ru>
2020-05-02 22:42:41 +0300

    n/a

--- models/CensoringQuery.php
+++ models/CensoringQuery.php
@@ -7,7 +7,7 @@ class CensoringQuery extends \yii\db\ActiveQuery
 
     public function createdDate($date)
     {
-        $time = strtotime($date /*. ' 00:00:00'*/);
+        $time = strtotime($date . ' 00:00:00');
 
         return $this->andWhere('YEAR(created)=:year AND MONTH(created)=:month AND DAY(created)=:day', [
             ':year' => date('Y', $time),
